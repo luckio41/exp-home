8563c0180ed6ba9335f57af0c445e9732b40db4d
Esteban Muñoz Lagos <emunoz.ccp@gmail.com>
2016-05-17 21:21:19 -0400

    Plantilla integrada

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require("express");
+var path = require('path');
 var app = express();
 
-app.get("/", function(req, res) {
-    res.send("Luckio Home Page");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.get('/', function(req, res){
+  res.render('layout');
 });
 
 app.listen(80);
